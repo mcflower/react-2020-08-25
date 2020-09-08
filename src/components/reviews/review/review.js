ca17c95f2efe7f9390afaa7b743b233121313c60
ZELENSKIY Ilya <ilya.zelenskiy@vaz.ru>
2020-09-08 14:18:56 +0400

    remove propTypes

--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Rate from '../../rate';
 import styles from './review.module.css';
+import PropTypes from 'prop-types';
 
 const Review = ({ user, text, rating }) => (
   <div className={styles.review}>
